fe1b3ff8eddcf571c3eae65222850cb2212f2c83
Mitchell Vollmer <mitchellvollmer@shaw.ca>
2023-03-12 23:20:04 -0600

    Set up user interactions

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -5,10 +5,42 @@
  */
 package services;
 
+import models.Role;
+import models.User;
+import dataaccess.UserDB;
+
+import java.util.List;
+
 /**
  *
  * @author Mitchell
  */
 public class UserService {
-    
+
+    public static void insert(String email, String firstName, String lastName, String password, Role role) {
+        User user = new User(email, firstName, lastName, password, role);
+        UserDB.insert(user);
+    }
+
+    public static void update(String email, String firstName, String lastName, String password, Role role) throws Exception {
+        User user = new User(email, firstName, lastName, password, role);
+        UserDB.update(user);
+    }
+
+    public static void delete(String email) {
+        User user = new User();
+        user.setEmail(email);
+        UserDB.delete(user);
+    }
+
+    public static User get(String email) {
+        return UserDB.get(email);
+    }
+
+    public static List<User> getAll() {
+        return UserDB.getAll();
+    }
+
+
+
 }
